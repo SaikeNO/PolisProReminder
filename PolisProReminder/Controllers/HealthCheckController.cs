fb18e3a7e01aaf7dfd87448afeef92c25a09a527
Mateusz Lengiewicz <mateusz.lengiewicz111@gmail.com>
2024-08-05 20:25:45 +0200

    add health check controller

---
+++ PolisProReminder/Controllers/HealthCheckController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PolisProReminder.API.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthCheckController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult HealthCheck()
+    {
+        return Ok(DateTime.UtcNow);
+    }
+}
