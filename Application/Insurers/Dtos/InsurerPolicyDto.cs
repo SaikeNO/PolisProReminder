1e5ed4b023cbce063a9b9104d5bba4a05cdba9e9
Mateusz Lengiewicz <mateusz.lengiewicz111@gmail.com>
2024-10-11 19:57:47 +0200

    add insuranceCompany reference in InsurerPolicyDto

--- Application/Insurers/Dtos/InsurerPolicyDto.cs
+++ Application/Insurers/Dtos/InsurerPolicyDto.cs
@@ -1,4 +1,5 @@
-﻿using PolisProReminder.Application.InsuranceTypes.Dtos;
+﻿using PolisProReminder.Application.InsuranceCompanies.Dtos;
+using PolisProReminder.Application.InsuranceTypes.Dtos;
 
 namespace PolisProReminder.Application.Insurers.Dtos;
 
@@ -7,7 +8,7 @@ public class InsurerPolicyDto
     public Guid Id { get; set; }
     public string Title { get; set; } = null!;
     public string PolicyNumber { get; set; } = null!;
-    public string InsuranceCompany { get; set; } = null!;
+    public InsuranceCompanyDto InsuranceCompany { get; set; } = null!;
     public DateOnly StartDate { get; set; }
     public DateOnly EndDate { get; set; }
     public DateOnly PaymentDate { get; set; }
