67355645dc7ed95dacb336f07b5deac3f7a36225
Mateusz Lengiewicz <mateusz.lengiewicz111@gmail.com>
2024-11-08 22:54:14 +0100

    add ChangeOrderCommand for todoTasks

---
+++ Application/TodoTasks/Commands/ChangeOrder/ChangeOrderCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using PolisProReminder.Application.Users;
+using PolisProReminder.Domain.Repositories;
+
+namespace PolisProReminder.Application.TodoTasks.Commands.ChangeOrder;
+
+internal class ChangeOrderCommandHandler(IUserContext userContext, ITodoTasksRepository todoTasksRepository) : IRequestHandler<ChangeOrderCommand>
+{
+    private readonly ITodoTasksRepository _todoTasksRepository = todoTasksRepository;
+    private readonly IUserContext _userContext = userContext;
+    public async Task Handle(ChangeOrderCommand request, CancellationToken cancellationToken)
+    {
+        var currentUser = _userContext.GetCurrentUser();
+        _ = currentUser ?? throw new InvalidOperationException("Current User is not present");
+
+        foreach (var requestTask in request.TodoTasks)
+        {
+            var task = await _todoTasksRepository.Get(currentUser.Id, requestTask.Id);
+            if (task == null) continue;
+
+            task.Order = requestTask.Order;
+        }
+    }
+}
---
+++ Application/TodoTasks/Commands/ChangeOrder/ChangeOrderCommandValidator.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using PolisProReminder.Application.TodoTasks.Dtos;
+
+namespace PolisProReminder.Application.TodoTasks.Commands.ChangeOrder;
+
+internal class ChangeOrderCommandValidator : AbstractValidator<ChangeOrderCommand>
+{
+    public ChangeOrderCommandValidator()
+    {
+        RuleFor(command => command.TodoTasks)
+            .NotNull().WithMessage("TodoTasks cannot be null.")
+            .NotEmpty().WithMessage("TodoTasks cannot be empty.");
+
+        RuleForEach(command => command.TodoTasks).ChildRules(todoTask =>
+        {
+            todoTask.RuleFor(task => task.Id)
+                .NotEmpty().WithMessage("Task ID cannot be empty.");
+
+            todoTask.RuleFor(task => task.Title)
+                .NotEmpty().WithMessage("Task title cannot be empty.")
+                .MaximumLength(250).WithMessage("Task title cannot exceed 100 characters.");
+
+            todoTask.RuleFor(task => task.Order)
+                .GreaterThanOrEqualTo(0).WithMessage("Task order must be zero or greater.");
+        });
+
+        RuleFor(command => command.TodoTasks)
+            .Must(HaveUniqueOrder).WithMessage("Each task must have a unique order.")
+            .Must(HaveUniqueIds).WithMessage("Each task must have a unique ID.");
+    }
+
+    private bool HaveUniqueOrder(IEnumerable<TodoTaskDto> todoTasks)
+    {
+        var orderList = todoTasks.Select(task => task.Order);
+        return orderList.Distinct().Count() == orderList.Count();
+    }
+
+    private bool HaveUniqueIds(IEnumerable<TodoTaskDto> todoTasks)
+    {
+        var idList = todoTasks.Select(task => task.Id);
+        return idList.Distinct().Count() == idList.Count();
+    }
+}
